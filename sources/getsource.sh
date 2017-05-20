223ca563c5c8b7fa128a6005db4a08e50eb0f9c7
sling00 <sling00@gmail.com>
2017-05-20 05:25:50 -0500

    add packaging stuffs

--- sources/getsource.sh
+++ sources/getsource.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "This will download the afulnx binary from the AMI Bios official website and extract the binary components into the correct position within the git tree.
+AFUVERSION="5.05.04"
+URL="https://www.wimsbios.com/files/flashers/ami/afulnx/afulnx-$AFUVERSION.zip
+MD5="69703a97a727d5c94662f214c1b8c158"
+OUTPUTFILE="/tmp/afulnx-$AFUVERSION.zip"
+
+
 
